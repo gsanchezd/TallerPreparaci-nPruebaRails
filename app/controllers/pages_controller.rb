ba0bf74b40f0a050e87df4c35dd009153c2b9563
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2016-10-17 21:42:53 -0300

    rest of the proyect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def x
   end
+
+  def save_user
+    User.create(name: params[:name], age: params[:age], email: params[:email])
+    redirect_to pages_x_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/x'
+  post 'pages/save_user'
+
   root 'pages#x'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
